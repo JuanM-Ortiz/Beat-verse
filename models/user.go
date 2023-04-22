622603b1b8f93b44b0042f8aaabbc6c851748db8
JuanM-Ortiz <juanmanuel.ortiz.vaz@gmail.com>
2023-04-22 18:02:47 -0300

    refactor

--- models/user.go
+++ models/user.go
@@ -9,13 +9,13 @@ import (
 type User struct {
 	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name        string             `bson:"name" json:"name,omitempty"`
-	LastName    string             `bson:"lastName" json:"lastname,omitempty"`
-	DateOfBirth time.Time          `bson:"dateOfBirth" json:"dateOfBirth,omitempty"`
+	LastName    string             `bson:"lastname" json:"lastname,omitempty"`
+	DateOfBirth time.Time          `bson:"dateofbirth" json:"dateofbirth,omitempty"`
 	Email       string             `bson:"email" json:"email"`
 	Password    string             `bson:"password" json:"password,omitempty"`
 	Avatar      string             `bson:"avatar" json:"avatar,omitempty"`
 	Banner      string             `bson:"banner" json:"banner,omitempty"`
 	Biography   string             `bson:"biography" json:"biography,omitempty"`
 	Location    string             `bson:"location" json:"location,omitempty"`
-	WebSite     string             `bson:"webSite" json:"webSite,omitempty"`
+	WebSite     string             `bson:"website" json:"website,omitempty"`
 }
